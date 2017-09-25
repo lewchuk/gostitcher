041fb39379695b6c143433a764bb71f21f2582e3
Stephen Lewchuk <stephen.lewchuk@gmail.com>
2017-09-24 18:08:53 -0700

    Load images based on config file

--- gostitcher.go
+++ gostitcher.go
@@ -3,10 +3,20 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"image/color"
+	"image/jpeg"
 	"io/ioutil"
 	"os"
 	"path"
 )
+
+// https://space.stackexchange.com/questions/12510/cassinis-camera-continuum-band-filters
+// A map of filter names to effective wavelengths.
+var FilterMap = map[string]int{
+	"BL1": 463,
+	"GRN": 568,
+	"RED": 647,
+}
 
 // The type for marshalling a config.json file from a folder with images.
 type ImageConfig struct {
@@ -37,6 +47,28 @@ func main() {
 		os.Exit(-1)
 	}
 
-	fmt.Println(config.Files[0].Filename)
-	fmt.Println(config.Files[0].Filter)
+	for _, imageConfig := range config.Files {
+		fmt.Println("Processing: ", imageConfig.Filename)
+		fullPath := path.Join(inputPath, imageConfig.Filename)
+
+		f, err3 := os.Open(fullPath)
+		if err3 != nil {
+			fmt.Println("Error reading iamge:", err3)
+			os.Exit(1)
+		}
+
+		image, err4 := jpeg.Decode(f)
+
+		if err4 != nil {
+			fmt.Println("Error decoding image:", err4)
+			os.Exit(1)
+		}
+
+		if image.ColorModel() != color.GrayModel {
+			fmt.Println("Image not grayscale, can't convert")
+			os.Exit(1)
+		}
+
+		fmt.Println(image.Bounds())
+	}
 }
