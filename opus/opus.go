b8341f0b460e83854e1b2afd4fa0bf02c2c5fa88
Stephen Lewchuk <stephen.lewchuk@gmail.com>
2017-11-21 22:44:20 -0800

    Process API response to an array of data with timestamps

--- opus/opus.go
+++ opus/opus.go
@@ -4,9 +4,104 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
 	cleanhttp "github.com/hashicorp/go-cleanhttp"
 )
+
+type OpusAPIResponse struct {
+	PageNo  int        `json:"page_no"`
+	Columns []string   `json:"columns"`
+	Images  [][]string `json:"page"`
+}
+
+type OpusImage struct {
+	RingObsId string
+	Time      time.Time
+	Filter    string
+}
+
+func getAPIResponse(request *http.Request) (OpusAPIResponse, error) {
+	client := cleanhttp.DefaultClient()
+
+	var data OpusAPIResponse
+
+	resp, err := client.Do(request)
+
+	if err != nil {
+		return data, fmt.Errorf("requesting from %s: %s", request.URL, err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return data, fmt.Errorf("loading api response %s: %s", request.URL, err)
+	}
+
+	data = OpusAPIResponse{}
+
+	if err := json.Unmarshal(body, &data); err != nil {
+		return data, fmt.Errorf("parsing api response %s: %s", body, err)
+	}
+
+	return data, nil
+}
+
+func findIndex(val string, content []string) int {
+	index := -1
+
+	for i, v := range content {
+		if val == v {
+			index = i
+			break
+		}
+	}
+
+	return index
+}
+
+func translateAPIResonse(data OpusAPIResponse) ([]OpusImage, error) {
+	idIndex := findIndex("Ring Observation ID", data.Columns)
+	timeIndex := findIndex("Observation Time 1 (UTC)", data.Columns)
+	filterIndex := findIndex("Filter", data.Columns)
+
+	if idIndex == -1 || timeIndex == -1 || filterIndex == -1 {
+		return nil, fmt.Errorf(
+			"Getting column indexes from %s (id: %d, time: %d, filter: %d)",
+			data.Columns, idIndex, timeIndex, filterIndex)
+	}
+
+	images := make([]OpusImage, len(data.Images))
+	for i, imgArray := range data.Images {
+		timeS := imgArray[timeIndex]
+		// Replace the day of year with a dummy value singe golang time doesn't support day of year.
+		dayOfYear, err := strconv.Atoi(timeS[5:8])
+		if err != nil {
+			return nil, fmt.Errorf("parsing day of year from %s: %s", timeS, err)
+		}
+		dayOfYearDuration := time.Duration(dayOfYear*24) * time.Hour
+		dayStripped := strings.Replace(timeS, timeS[4:8], "-01", 1)
+		timeTaken, err := time.Parse("2006-02T15:04:05.000", dayStripped)
+		dateTimeTaken := timeTaken.Add(dayOfYearDuration)
+
+		if err != nil {
+			return nil, fmt.Errorf("parsing time %s: %s", imgArray[timeIndex], err)
+		}
+
+		images[i] = OpusImage{
+			RingObsId: imgArray[idIndex],
+			Time:      dateTimeTaken,
+			Filter:    imgArray[filterIndex],
+		}
+	}
+
+	return images, nil
+}
 
 func CombineImages() error {
 
@@ -19,7 +114,7 @@ func CombineImages() error {
 	enceladusTargetOpt := "target=ENCELADUS"
 	filterOpts := "FILTER=BL1,GRN,RED"
 	orderOpt := "order=time1"
-	colOpt := "cols=ringobsid,time1,time2,filter"
+	colOpt := "cols=ringobsid,time1,filter"
 	narrowCameraOpt := "camera=Narrow+Angle"
 	pageSizeOpt := "limit=99"
 
@@ -38,27 +133,25 @@ func CombineImages() error {
 		colOpt,
 		pageSizeOpt)
 
-	fmt.Println(queryURL)
+	request, err := http.NewRequest("GET", queryURL, nil)
 
-	client := cleanhttp.DefaultClient()
+	if err != nil {
+		return fmt.Errorf("creating request for %s: %s", queryURL, err)
+	}
 
-	resp, err := client.Get(queryURL)
+	data, err := getAPIResponse(request)
 
-	if (err != nil) { return err }
+	if err != nil {
+		return err
+	}
 
-	defer resp.Body.Close()
+	images, err := translateAPIResonse(data)
 
-	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
 
-	if (err != nil) { return err }
-
-	data := map[string]interface{}{}
-
-	err = json.Unmarshal(body, &data)
-
-	if (err != nil) { return err }
-
-	fmt.Println(data)
+	fmt.Println(images)
 
 	return nil
 }
